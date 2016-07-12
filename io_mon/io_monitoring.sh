895b5793e7e381eacedc2b98796e87ddf9fb3132
Yury Popov <popovy@netcracker.com>
2016-07-13 00:55:55 +0300

    io monitoring added

---
+++ io_mon/io_monitoring.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+str=`sar -b |tac|grep -m 16 '.'| awk 'NR>1 {bread += $6 ; bwrtn += $7; count++} END {print int(bread/count), int(bwrtn/count)}'`
+bread=$(echo $str | cut -f1 -d' ')
+bwrtn=$(echo $str | cut -f2 -d' ')
+
+if [[ "$bread" -gt 170000 || "$bwrtn" -gt 170000 ]]
+then echo -e "ALERT!\nbread = $bread\nbwrtn = $bwrtn"|mail -s "I/O Limit ALERT" dmitrii.martynov@netcracker.com
+fi
